88c6886236dcc499e139b24b7cf836ccc1550a30
zlx <zlx@omk.io>
2018-04-24 15:11:48 +0800

    p

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var server = require('http').createServer();
 var io = require('socket.io')(server);
 io.on('connection', function(client){
+    console.log('in')
     client.on('event', function(data){
         console.log(data, 'data111')
     });
