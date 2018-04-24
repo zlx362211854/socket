9254c0546b7d8abd0739ede1af4ad81f796249d2
zlx <zlx@omk.io>
2018-04-24 15:20:31 +0800

    message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var server = require('http').createServer();
 var io = require('socket.io')(server);
 io.on('connection', function(client){
     console.log('in')
-    client.on('event', function(data){
+    client.on('message', function(data){
         console.log(data, 'data111')
     });
     client.emit('news', { hello: 'world' });
